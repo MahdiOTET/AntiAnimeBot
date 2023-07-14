54436a59e0455bd9b897daa51c46ec28af4bbd3c
MahdiOTET <mahdiimami@yahoo.com>
2023-07-14 16:42:59 +0330

    Testing a sample

--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,33 @@
+#include <iostream>
 #include <tgbot/tgbot.h>
 
-int main() {
-	TgBot::Bot bot("6351217913:AAEIptkNU78Zz2f9IyZlTIR2ZSm_S9eFVEg");
+int main() 
+{
+    TgBot::Bot bot("6351217913:AAEIptkNU78Zz2f9IyZlTIR2ZSm_S9eFVEg");
 
-	bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
-		{
-			bot.getApi().sendMessage(message->chat->id, "Hi!");
-		});
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) 
+        {
+        bot.getApi().sendMessage(message->chat->id, "Hello, world!");
+        });
 
-	bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
-		{
-			printf("User wrote %s\n", message->text.c_str());
-			if (StringTools::startsWith(message->text, "/start"))
-			{
-				return;
-			}
-			bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-		});
-	try 
-	{
-		printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-		TgBot::TgLongPoll longPoll(bot);
-		while (true) 
-		{
-			printf("Long poll started\n");
-			longPoll.start();
-		}
-	}
-	catch (TgBot::TgException& e) 
-	{
-		printf("error: %s\n", e.what());
-	}
-	return 0;
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) 
+        {
+        std::cout << "User wrote " << message->text << std::endl;
+        });
+
+    try {
+        std::cout << "Bot username: " << bot.getApi().getMe()->username << std::endl;
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) 
+        {
+            std::cout << "Long poll started" << std::endl;
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) 
+    {
+        std::cerr << "error: " << e.what() << std::endl;
+    }
+    system("pause");
+    return 0;
 }
